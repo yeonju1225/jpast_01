ed6d1d8ad3be3ec015367fcde166e18a4e6f8385
yeonj99 <yeonj99@naver.com>
2022-12-06 12:22:49 +0900

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,54 @@
+package org.example;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class Main {
+    public static void main(String[] args) {
+        //엔티티 메니저 : 웹서버가 올라오는 시점에 DB당 하나만 생성.
+        //쓰레드간에 공유 절대 금지 ( 사용하고 버려야 함 )
+        //모든 데이터 변경은 트랜잭션 안에서 실행.
+        EntityManagerFactory emf =  Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em =  emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            /*
+            //신규 등록
+            Member member = new Member();
+            member.setId(2L);
+            member.setName("전명진");
+            em.persist(member);
+            */
+
+            /*
+            //조회
+            Member findMember = em.find(Member.class, 1L);
+            System.out.println("findMember.id = " + findMember.getId());
+            System.out.println("findMember.name = " + findMember.getName());
+            */
+
+            /*
+            //수정
+            Member findMember = em.find(Member.class, 1L);
+            findMember.setName("김유경");
+            */
+
+            //삭제
+            Member findMember = em.find(Member.class, 1L);
+            em.remove(findMember);
+
+            tx.commit();
+        } catch (Exception ex) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
---
+++ src/main/java/org/example/Member.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+//테이블 명이 USER라고 되있으면 이렇게.
+//@Table(name = "USER")
+public class Member {
+
+    @Id
+    private Long id;
+
+    //컬럼명이 다를때
+    //@Column(name = "username")
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
